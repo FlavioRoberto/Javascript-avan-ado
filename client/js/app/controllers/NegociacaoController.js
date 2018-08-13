0694faae32ba866714cbf2b978f6d06b86046761
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2018-08-13 14:06:27 -0300

    Iniciando com Data Bind

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,32 +5,40 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._inputData = $('#data');
-        this._listaNegociacoes = new ListaNegociacoes();
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-        this._mensagem = new Mensagem();
+
+        let self = this;
         this._mensagemView = new MensagemView($('#MensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(),
+            this._negociacoesView,
+            ['adiciona', 'esvazia']);
+
+        this._mensagem = new Bind(new Mensagem(),
+            this._mensagemView,
+            ['texto']);
+
     }
 
     adiciona(event) {
         event.preventDefault();
 
         let data = DataHelper.textoParaData(this._inputData.value);
-            
+
         this._listaNegociacoes.adiciona(this._criaNegociacao(data));
 
         this._mensagem.texto = "Negociação adicionada com sucesso."
-
-        this._mensagemView.update(this._mensagem);
-
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._limpaFormulario();
 
     }
 
-    _criaNegociacao(data){
+    esvazia() {
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Lista de negociações apagadas';
+    }
+
+    _criaNegociacao(data) {
         return new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
     }
 
